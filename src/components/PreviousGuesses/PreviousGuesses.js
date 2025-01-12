e8064d8a5cb1bf74b0d9c8c03f6a507d0e72ad73
Vasilis Tsirimokos <Silisav@users.noreply.github.com>
2025-01-12 21:56:42 +0100

    Exercise 3

--- src/components/PreviousGuesses/PreviousGuesses.js
+++ src/components/PreviousGuesses/PreviousGuesses.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import Guess from '../Guess';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import { range } from '../../utils';
 
 function PreviousGuesses({ guesses }) {
   return (
-    <div className="guess-results">
-      {guesses.map((guess) => (
-        <p key={Math.random()} className="guess">
-          {guess}
-        </p>
-      ))}
-    </div>
+    <>
+      <div className="guess-results">
+        {range(NUM_OF_GUESSES_ALLOWED).map((i) => (
+          <Guess key={Math.random()} guess={guesses[i]} />
+        ))}
+      </div>
+    </>
   );
 }
 
