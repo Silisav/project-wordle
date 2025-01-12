cd36fb9f658e93cd598cf83c4693c6187465b6cc
Vasilis Tsirimokos <Silisav@users.noreply.github.com>
2025-01-12 17:49:51 +0100

    Exercise 1

---
+++ src/components/FormInput/FormInput.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function FormInput() {
+  const [guess, setGuess] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (guess.length !== 5) {
+      alert('Guess must be 5 letters long.');
+      return;
+    }
+    console.log({ guess });
+    setGuess('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        onChange={(event) => {
+          const nextGuess = event.target.value.toUpperCase();
+          setGuess(nextGuess);
+        }}
+        id="guess-input"
+        type="text"
+        pattern="[a-zA-Z]{5}"
+        value={guess}
+      />
+    </form>
+  );
+}
+
+export default FormInput;
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FormInput from '../FormInput';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
@@ -9,7 +10,11 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  return (
+    <>
+      <FormInput />
+    </>
+  );
 }
 
 export default Game;
